530adb6875b5d36d5f2b38f2396ba518daf754fe
Haasika Pasham <pasham2@illinois.edu>
2025-02-10 19:47:34 +0000

    Complete 2/10 hw

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,29 @@
 #include "solution.hpp"
 
-Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
-  return Student{};
+using namespace std;
+
+Student ReadStudentRecFromStream(std::istream& file) {
+  string full_name;
+  unsigned int uin = 0;
+  double gpa = 0.0;
+  string last;
+  string uins;
+  string gpas;
+
+  file >> full_name >> last >> uins >> gpas;
+
+  if (last[last.size() - 1] != ',') return Student{};
+  last.erase(last.size() - 1);
+  full_name += " " + last;
+
+  if (uins[uins.size() - 1] != ',') return Student{};
+  try {
+    uins.erase(uins.size() - 1);
+    uin = stoi(uins);
+    gpa = stoi(gpas);
+  } catch (...) {
+    return Student{};
+  }
+
+  return Student{full_name, uin, gpa};
 }
